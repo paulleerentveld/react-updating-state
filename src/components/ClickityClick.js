a530359da5a8d01f7fee95961b77290e0f809b7f
paulleerentveld <paul@veldnet.com.au>
2022-05-09 12:39:42 +1000

    lab

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,33 @@
 // Code ClickityClick Component Here
+import React from "react";
+import LightSwitch from "./LightSwitch";
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      hasBeenClicked: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      hasBeenClicked: true
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : "not"} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+        <LightSwitch />
+      </div>
+    );
+  }
+}
+
+export default ClickityClick;
+
